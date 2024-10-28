ae5ae15b3cdeaf2c5b0a5242bdbf4ead0b2231b1
hieutt66 <tranhieu.dev@gmail.com>
2024-10-28 23:17:48 +0700

    change to run build

--- src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -1,7 +1,6 @@
 package com.example.demo.model.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class CreateUserRequest {
 
     @JsonProperty
--- src/main/java/com/example/demo/services/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/services/CustomUserDetailsService.java
@@ -1,5 +1,4 @@
 package com.example.demo.services;
-
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
-
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -19,11 +16,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User appUser = userRepository.findByUsername(username);
-
         if (appUser == null) {
             throw new UsernameNotFoundException("User not found");
         }
-
         return new org.springframework.security.core.userdetails.User(
                 appUser.getUsername(), appUser.getPassword(), new ArrayList<>());
     }
